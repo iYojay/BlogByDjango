1a02f48e98e3107d48864fc8cd7606846bc96b1d
iYaojian <iyaojian@126.com>
2020-01-11 16:11:27 +0800

    add settings.production,

--- FirstDjango/settings/production.py
+++ FirstDjango/settings/production.py
@@ -15,10 +15,10 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dbtest',
+        'NAME': 'myblog',
         'USER': 'root',
         'PASSWORD': 'mysql@123.com',
         'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'PORT': '8018',
     }
 }
