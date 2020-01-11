3cb8b3376f2b898b7cf4076990b598a81191c728
iYaojian <iyaojian@126.com>
2020-01-11 16:00:26 +0800

    add path('',include('blog.urls')),

--- FirstDjango/wsgi.py
+++ FirstDjango/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FirstDjango.settings')
+os.environ.setdefault('FirstDjango.settings.production', 'FirstDjango.settings')
 
 application = get_wsgi_application()
